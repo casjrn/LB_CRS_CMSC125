3a8f2bae9772035f4ebb601f91bc4c2cc6d6e271
Chrystie <casajorne@up.edu.ph>
2026-02-12 00:43:50 +0800

    feat: implemented shell input

--- lab1/mysh.c
+++ lab1/mysh.c
@@ -1,6 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
 #include "mysh.h"
 
-int main (){
+void reap_background_jobs() {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
 
-    printf("Hello World!\n");
+int main() {
+    char line[MAX_LINE];
+    Command cmd;
+
+    while (1) {
+        reap_background_jobs();
+
+        printf("mysh> ");
+        fflush(stdout);
+
+        if (!fgets(line, sizeof(line), stdin))
+            break;
+
+        if (strlen(line) <= 1) continue;
+
+        if (parse_command(line, &cmd))
+            execute_command(&cmd);
+    }
+
+    return 0;
 }
--- lab1/mysh.h
+++ lab1/mysh.h
@@ -4,5 +4,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define MAX_LINE 1024
+#define MAX_JOBS 100
+
+typedef struct {
+    char *command;
+    char *args[256];
+    char *input_file;
+    char *output_file;
+    bool append;
+    bool background;
+} Command;
+
+int parse_command(char *line, Command *cmd);
+void free_command(Command *cmd);
+
+int execute_command(Command *cmd);
+
+void reap_background_jobs();
 
 #endif
