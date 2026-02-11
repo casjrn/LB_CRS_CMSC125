9feffe8966d1e4024e1955e42c69abe95aeeba5b
rykonnen <lqblancaflor@up.edu.ph>
2026-02-12 00:40:15 +0800

    feat: implement built in commands exit, cd, and pwd

---
+++ lab1/interpreter.c
@@ -0,0 +1,64 @@
+//put in header later
+#include <stdio.h>
+#include <string.h>
+
+// execution of commands
+
+
+void built_in_command(Command *cmd);
+void extern_command(Command *cmd);
+void io_redirection(Command *cmd);
+void background_exe(Command *cmd);
+
+
+
+void built_in_command(Command* cmd){
+    //EXIT
+    if (strcmp(cmd->command, "exit") == 0){
+        exit(0);
+
+    } 
+
+    //CD
+    else if (strcmp(cmd->command, "cd") == 0){
+
+        if (cmd->args[1] == NULL) {
+            fprintf(stderr, "mysh: cd: missing argument\n"); 
+
+        } else {
+                // path does not exist
+            if (chdir(cmd->args[1]) != 0) {
+                perror("mysh: cd"); 
+            }
+        }
+
+    } 
+    
+    //PWD
+    else if (strcmp(cmd->command, "pwd") == 0){
+        char *cwd = getcwd(NULL, 0);                //dangerous stuff
+        if (cwd != NULL) {
+            printf("%s\n", cwd);
+            free(cwd);                              //free memory
+        } else {
+            perror("mysh: pwd");
+        }
+    }
+
+}
+
+void extern_command(Command *cmd){
+
+
+}
+
+void io_redirection(Command *cmd){
+
+
+}
+
+
+void background_exe(Command *cmd){
+
+    
+}
