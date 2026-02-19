acfc79c7fdcbc8d3dd3fcaa6017f514e2060cbfe
rykonnen <lqblancaflor@up.edu.ph>
2026-02-19 15:13:07 +0800

    refactor: merged header file of lmb-branch with crs_branch

--- lab1/mysh.h
+++ lab1/mysh.h
@@ -4,23 +4,11 @@
 //put in header 
 #include <stdio.h>
 #include <string.h>
-<<<<<<< HEAD
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-
-//execution of commands
-void built_in_command(Command *cmd);
-void extern_command(Command *cmd);
-void io_redirection(Command *cmd);
-void background_exe(Command *cmd);
-
-
-//parser
-int parse_command(char *line, Command *cmd);
-=======
-#include <stdbool.h>
 
 #define MAX_LINE 1024
 #define MAX_JOBS 100
@@ -36,10 +24,7 @@ typedef struct {
 
 int parse_command(char *line, Command *cmd);
 void free_command(Command *cmd);
-
 int execute_command(Command *cmd);
-
 void reap_background_jobs();
->>>>>>> 3a8f2ba (feat: implemented shell input)
 
 #endif
