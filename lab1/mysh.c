ae7ba60ec9bba084e379a13529983e449afe6e8f
Chrystie <casajorne@up.edu.ph>
2026-02-19 15:46:03 +0800

    refactor: removed command argument in parser function

--- lab1/mysh.c
+++ lab1/mysh.c
@@ -1,6 +1,30 @@
+#include <sys/wait.h>
 #include "mysh.h"
 
-int main (){
+Command cmd;
 
-    printf("Hello World!\n");
+void reap_background_processes() {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+int main() {
+    char line[MAX_LINE];
+
+    while (1) {
+        reap_background_processes();
+        
+        printf("mysh> ");
+        fflush(stdout);
+
+        if (!fgets(line, sizeof(line), stdin))
+            break;
+
+        if (strlen(line) <= 1) continue;
+
+        if (parse_command(line)) {
+           built_in_command(&cmd);
+        }
+
+    return 0;
+    }
 }
