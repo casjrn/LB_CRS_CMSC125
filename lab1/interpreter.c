989d5a924998bd9bffade9a96bf0350fd60fcf57
rykonnen <lqblancaflor@up.edu.ph>
2026-02-21 01:05:17 +0800

    feat: implemented external commands

--- lab1/interpreter.c
+++ lab1/interpreter.c
@@ -1,67 +1,122 @@
 #include "mysh.h"
 
-void built_in_command(Command* cmd){
-    //EXIT
-    if (strcmp(cmd->command, "exit") == 0){
+void built_in_command(Command cmd)
+{
+
+    if (cmd.command == NULL)
+        return; // empty
+
+    // EXIT
+    else if (strcmp(cmd.command, "exit") == 0)
+    {
         exit(0);
+    }
 
-    } 
+    // CD
+    else if (strcmp(cmd.command, "cd") == 0)
+    {
 
-    //CD
-    else if (strcmp(cmd->command, "cd") == 0){
-
-        if (cmd->args[1] == NULL) {
-            fprintf(stderr, "mysh: cd: missing argument\n"); 
-
-        } else {
-                // path does not exist
-            if (chdir(cmd->args[1]) != 0) {
-                perror("mysh: cd"); 
+        if (cmd.args[1] == NULL)
+        {
+            fprintf(stderr, "mysh: cd: missing argument\n");
+        }
+        else
+        {
+            // path does not exist
+            if (chdir(cmd.args[1]) != 0)
+            {
+                perror("mysh: cd");
             }
         }
+    }
 
-    } 
-    
-    //PWD
-    else if (strcmp(cmd->command, "pwd") == 0){
-        char *cwd = getcwd(NULL, 0);                //dangerous stuff
-        if (cwd != NULL) {
+    // PWD
+    else if (strcmp(cmd.command, "pwd") == 0)
+    {
+        char *cwd = getcwd(NULL, 0); // dangerous stuff
+        if (cwd != NULL)
+        {
             printf("%s\n", cwd);
-            free(cwd);                              //free memory
-        } else {
+            free(cwd); // free memory
+        }
+        else
+        {
             perror("mysh: pwd");
         }
     }
-
 }
 
-void extern_command(Command *cmd){
+void extern_command(Command cmd)
+{
     pid_t pid = fork();
 
-    if (pid < 0) {
-        perror("fork failed");
-    } else if (pid == 0) {                          // CHILD
-        io_redirection(cmd);            
-        execvp(cmd->command, cmd->args); 
+    if (pid < 0)
+    { // PARENT
+        background_exe(cmd, pid);
+    }
+    else if (pid == 0)
+    { // CHILD
+        io_redirection(cmd);
+        
+        //execute
+        execvp(cmd.command, cmd.args);
         perror("exec failed");
         exit(127);
-    } else {                                        // PARENT
-        background_exe(cmd, pid);       
+    }
+    else
+    {
+        perror("fork failed");
     }
 }
 
+void io_redirection(Command cmd)
+{
+    if (cmd.input_file)
+    {
+        int fd = open(cmd.input_file, O_RDONLY);
 
-void io_redirection(Command *cmd){
+        if (fd < 0)
+        {
+            perror("open input");
+            exit(1);
+        }
+
+        dup2(fd, STDIN_FILENO);
+        close(fd);
+    }
+
+    if (cmd.output_file)
+    {
+        int flags = O_WRONLY | O_CREAT | (cmd.append ? O_APPEND : O_TRUNC);
+        int fd = open(cmd.output_file, flags, 0644);
+
+        if (fd < 0)
+        {
+            perror("open output");
+            exit(1);
+        }
+
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+    }
+}
+
+void background_exe(Command cmd, pid_t pid)
+{
+    if (!cmd.background) {
+        int status;
+        waitpid(pid, &status, 0); // Parent pauses here
+        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            printf("Command exited with code %d\n", WEXITSTATUS(status));
+        }
+    } else {
+        // Parent does NOT wait
+        printf("[%d] Started: %s (PID: %d)\n", job_id++, cmd.command, pid);
+    }
 
 }
 
-
-
-void background_exe(Command *cmd){
-    
-}
-
-
-void reap_background_processes() {
+void reap_background_processes()
+{
     while (waitpid(-1, NULL, WNOHANG) > 0);
 }
